9bbe1c51fcb688017c593988ede472cac4857111
ashurik522 <ashurik522@gmail.com>
2022-05-19 11:12:28 -0400

    footer styling update

--- src/Footer.js
+++ src/Footer.js
@@ -1,23 +1,41 @@
-import axios from "axios";
+
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import styled from "styled-components";
 
 
+const StyledFooter = styled.div`
+    font-size: 2rem;
+    color: white;
+    
+   
 
+    footer{
+        display: flex;
+        justify-content: center;
+    }
+
+    .icon{
+        padding: 10px;
+    }
+
+`
 
 
 export default function Footer(){
 
     return (
+        <StyledFooter>
         <footer>
-            <div className="linkedIn">
+            <div className="icon">
                 <FontAwesomeIcon icon={faLinkedin} />
             </div>
-            <div className="gitHub">
+            <div className="icon">
                 <FontAwesomeIcon icon={faGithub} />
             </div>
         </footer>
+        </StyledFooter>
     )
 }
 
